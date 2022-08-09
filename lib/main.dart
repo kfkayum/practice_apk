717f25e5f50b7498c055613ea94439bb8b33868a
kfkayum <kfkayum@gmail.com>
2022-08-10 01:18:21 +0600

    add Navigation Rail

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,71 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_final_fields
+
 import 'package:flutter/material.dart';
+import 'package:new_project/page1.dart';
+import 'package:new_project/page2.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+  final _page = [
+    One(),
+    Two(),
+  ];
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Data Table"),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: DataTable(
-            columns: const [
-              DataColumn(label: Text("Name")),
-              DataColumn(label: Text("Age")),
-              DataColumn(label: Text("Roll")),
-              DataColumn(label: Text("Class")),
-            ],
-            rows: const [
-              DataRow(cells: [
-                DataCell(Text('Kayum')),
-                DataCell(Text('25')),
-                DataCell(Text('20')),
-                DataCell(Text('CSE')),
-              ]),
-              DataRow(cells: [
-                DataCell(Text('Kayum')),
-                DataCell(Text('25')),
-                DataCell(Text('20')),
-                DataCell(Text('CSE')),
-              ]),
-              DataRow(cells: [
-                DataCell(Text('Kayum')),
-                DataCell(Text('25')),
-                DataCell(Text('20')),
-                DataCell(Text('CSE')),
-              ]),
-              DataRow(cells: [
-                DataCell(Text('Kayum')),
-                DataCell(Text('25')),
-                DataCell(Text('20')),
-                DataCell(Text('CSE')),
-              ]),
-              DataRow(cells: [
-                DataCell(Text('Kayum')),
-                DataCell(Text('25')),
-                DataCell(Text('20')),
-                DataCell(Text('CSE')),
-              ]),
-              DataRow(cells: [
-                DataCell(Text('Kayum')),
-                DataCell(Text('25')),
-                DataCell(Text('20')),
-                DataCell(Text('CSE')),
-              ]),
-              DataRow(cells: [
-                DataCell(Text('Kayum')),
-                DataCell(Text('25')),
-                DataCell(Text('20')),
-                DataCell(Text('CSE')),
-              ]),
+        backgroundColor: Color.fromARGB(255, 219, 206, 241),
+        body: SafeArea(
+          child: Row(
+            children: [
+              NavigationRail(
+                labelType: NavigationRailLabelType.all,
+                selectedLabelTextStyle: TextStyle(color: Colors.black),
+                backgroundColor: Colors.deepPurpleAccent,
+                destinations: [
+                  const NavigationRailDestination(
+                    icon: Icon(
+                      Icons.wifi,
+                      color: Colors.white,
+                    ),
+                    label: Text('Wifi'),
+                    selectedIcon: Icon(
+                      Icons.wifi,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const NavigationRailDestination(
+                    icon: Icon(
+                      Icons.camera_alt_outlined,
+                      color: Colors.white,
+                    ),
+                    label: Text('Camera'),
+                    selectedIcon: Icon(
+                      Icons.camera_alt_outlined,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+                selectedIndex: _selectedIndex,
+                onDestinationSelected: (val) {
+                  setState(() {
+                    _selectedIndex = val;
+                  });
+                },
+              ),
+              Expanded(child: _page[_selectedIndex]),
             ],
           ),
         ),
